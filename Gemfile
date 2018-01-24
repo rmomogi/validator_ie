1e0979e607dad14961d6c8dbb4763576d6124266
Rodrigo Oliveira <rodrigo.oliveira@sage.com>
2018-01-24 19:43:47 +0000

    Fix validation MG

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in validator_ie.gemspec
 gemspec
+gem "activemodel", require: "active_model"
+gem "pry"
--- lib/validator_ie/core/state_mg.rb
+++ lib/validator_ie/core/state_mg.rb
@@ -20,10 +20,10 @@ module ValidatorIE
       self.number = number[0..2].concat("0").concat(number[3..12])
       matrix = number[0..number.size - 3].split(//).map.with_index {|m, k| k.next.odd? ? 1 : 2}
       result1 = number_should_modX(11, 11, nil, matrix, true)
-      
+            
       self.number = number[0..2].concat(number[4..13])
       result2 = number_should_modX(11, 11, 11, nil)
-      
+
       return if result1 && result2
       errors.add(:number, 'não é válido para inscrição estadual.')
 
@@ -37,10 +37,11 @@ module ValidatorIE
         return false if at > matrix.size - 1
         sum = numberX[0..at].map.with_index { |v, k| v * matrix[k]}.join('').split(//).map(&:to_i).reduce(&:+)
         digit = (sum - (sum.to_s.split(//).last.to_i - 10)) - sum
+        digit = (digit == 10 ? 0 : digit)        
       else
         sum = ModFunctions.sumX(numberX, at, final, true, matrix)
         divmod = sum.divmod(mod).last
-        digit = divmod < 2 ? 0 : mod - divmod
+        digit = divmod < 2 ? 0 : mod - divmod        
       end
       numberX[at + 1].eql?(digit)
     end
